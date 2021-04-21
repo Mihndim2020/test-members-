d1d06dd0cbc6877de6909478904eb9d637ccd461
Mihndim <jelsieprisca2016@gmail.com>
2021-04-21 16:49:30 -0400

    Create index action in post controller, create corresponding view to show every post, add logic to show name and username only when the user is signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,17 +22,25 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
     end
+    # @post = Post.new(post_params)
+
+    # respond_to do |format|
+    #   if @post.save
+    #     format.html { redirect_to @post, notice: "Post was successfully created." }
+    #     format.json { render :show, status: :created, location: @post }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @post.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:new, :create, :index]
   devise_for :users
-  root to: "home#index"
+  #root to: "home#index"
+  root 'posts#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
