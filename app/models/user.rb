65c6c80460f521a68539bf92f8f634ec2780034f
Mihndim <jelsieprisca2016@gmail.com>
2021-04-21 13:34:42 -0400

    Add associations and validations to user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+
+  has_many :posts, dependent: :destroy 
+
+  validates :name, length: { minimum: 2 }
+  validates :username, length: { minimum: 2 }
+  validates :password, presence: true, length: { in: 6..20 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
 end
