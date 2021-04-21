cf3eaefbcf019ea29f1a0e078f47e9e10ed6c172
Mihndim <jelsieprisca2016@gmail.com>
2021-04-21 19:08:41 -0400

    Add registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy 
 
-  validates :name, length: { minimum: 2 }
-  validates :username, length: { minimum: 2 }
+  validates :name, presence: true,  length: { minimum: 2 }
+  validates :username, presence: true, length: { minimum: 2 }
   validates :password, presence: true, length: { in: 6..20 }
 
   # Include default devise modules. Others available are:
