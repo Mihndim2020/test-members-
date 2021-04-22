5a240469df5f3ffd02e9e2d2a457d21d58dff2b9
Mihndim <jelsieprisca2016@gmail.com>
2021-04-22 16:03:00 -0400

    Add helper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,35 @@
 module PostsHelper
+  def user_edit(_user)
+    link_to 'Edit Profile', edit_user_registration_path if user_signed_in?
+  end
+
+  def email(post)
+    @email = User.find_by(id: post.user_id).email if user_signed_in?
+    @email
+  end
+
+  def author(post)
+    @author = User.find_by(id: post.user_id).username if user_signed_in?
+    @author
+  end
+
+  def user_email(_user)
+    'Email' if user_signed_in?
+  end
+
+  def post_new(_user)
+    link_to 'New Post', new_post_url if user_signed_in?
+  end
+
+  def user_sign(_user)
+    link_to 'Sign In', new_user_session_path unless user_signed_in?
+  end
+
+  def user_author(_user)
+    'Author' if user_signed_in?
+  end
+
+  def user_logout(_user)
+    link_to 'Logout', destroy_user_session_path, method: :delete if user_signed_in?
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   resources :posts, only: [:new, :create, :index]
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   #root to: "home#index"
-  root 'posts#index'
+  root to: 'posts#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
